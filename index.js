c9231e49504297f6eb3c1e8cbd22ed0ed97e9a14
fesson777 <cccp0110@gmail.com>
2020-02-28 14:56:28 +0300

    1

--- index.js
+++ index.js
@@ -4,26 +4,77 @@ const fruits = [
     {id: 3, title: 'Манго', price: 40, img: 'https://itsfresh.ru/upload/iblock/178/178d8253202ef1c7af13bdbd67ce65cd.jpg'},
   ]
 
+const toHTML = fruit => `
+<div class="col">
+    <div class="card">
+        <img style = "width: 100%" src="${fruit.img}" class="card-img-top" alt="${fruit.title}">
+        <div class="card-body">
+            <h5 class="card-title">${fruit.title}</h5>
+            <p class="card-text"></p>
+            <a href="#" class="btn btn-primary" data-btn="price" data-id="${fruit.id}">Посмотреть цену</a>
+            <a href="#" class="btn btn-danger" data-btn="remove" data-id="${fruit.id}">Удалить</a>
+        </div>
+    </div>
+</div>
+`
 
+function render() {
+    const html = fruits.map(toHTML).join('')
+    document.querySelector('#fruits').innerHTML = html
+}
 
-const modal = $.modal({
-    title: 'my-modal',
-    closable: true,
-    content: `
-    <h4>Modal is working</h4>
-    <p>Lorem ipsum dolor sit</p>`
-    ,
+render()
+
+const priceModal = $.modal({
+    title: 'Цена на товар',
+    closable: true,    
     width: '400px',
     footerButtons: [
-        {text: 'ok', type: 'success', handler() {
-            console.log('success btm clicked')
-            modal.close()
-        }},
-        {text: 'Cancel', type: 'danger', handler() {
-            console.log('danger btm clicked')
-            modal.close()
-        }}
+        {text: 'Закрыть', type: 'success', handler() {
+           priceModal.close()
+        }}       
     ]
-
 })
 
+// const confirmModal = $.modal({
+//     title: 'Вы уверены?',
+//     closable: true,    
+//     width: '400px',
+//     footerButtons: [
+//         {text: 'Отменить', type: 'secondary', handler() {
+//             confirmModal.close()
+//         }},
+//         {text: 'Удалить', type: 'danger', handler() {
+//             confirmModal.close()
+//          }}         
+//     ]
+// })
+
+document.addEventListener('click', event => {
+    event.preventDefault()
+    const btnType = event.target.dataset.btn
+    const id = +event.target.dataset.id
+    const fruit = fruits.find(f => f.id === id)
+
+    if(btnType === 'price')  {
+        priceModal.setContent(`
+        <p>Цена на ${fruit.title}: <strong>${fruit.price}</strong>$</p>
+        `)
+        priceModal.open()       
+    } else if(btnType === 'remove') {
+       $.confirm({
+        title: 'Вы уверены?',
+        content: `<p>Вы удаляете фрукт : <strong>${fruit.title}</strong></p>`
+       }).then(()=>{
+           console.log('Remove')
+       }).catch( () => {
+        console.log('Cancel')
+       })
+        
+
+        // confirmModal.setContent(`
+        // <p>Вы удаляете фрукт : <strong>${fruit.title}</strong></p>
+        // `)
+        // confirmModal.open()
+    }
+})
